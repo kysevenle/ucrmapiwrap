7c750a477e95202b29aebd163e26880298d2055e
kysevenle <kysevenle@gmail.com>
2019-02-08 14:50:07 -0700

    created Services class with get_list_services and get_service_by_id meth

---
+++ ucrmapiwrap/services.py
@@ -0,0 +1,17 @@
+import requests
+
+from ucrmapiwrap.config_reader import config
+
+
+class Services:
+    def __init__(self):
+        self.url = config.BASE_URL + 'clients/services'
+        self.headers = {'Content-Type': 'application/json',
+                        'x-Auth-App-Key': config.WRITE_KEY}
+
+    def get_list_of_services(self, params=None):
+        return requests.get(self.url, headers=self.headers, params=params)
+
+    def get_service_by_id(self, id):
+        url = f'{self.url}/{id}'
+        return requests.get(url, headers=self.headers)
