0797ff2e2607b401f174019fe35419435bfde3ce
kysevenle <kysevenle@gmail.com>
2019-02-08 20:28:11 -0700

    changes to get services method

--- ucrmapiwrap/services.py
+++ ucrmapiwrap/services.py
@@ -8,10 +8,25 @@ class Services:
         self.url = config.BASE_URL + 'clients/services'
         self.headers = {'Content-Type': 'application/json',
                         'x-Auth-App-Key': config.WRITE_KEY}
+        self.batch = []
+        self.errors = {}
 
     def get_services_list(self, params=None):
-        return requests.get(self.url, headers=self.headers, params=params)
+        self.batch = requests.get(self.url, headers=self.headers, params=params).json()
 
     def get_service_by_id(self, id):
         url = f'{self.url}/{id}'
         return requests.get(url, headers=self.headers)
+
+    def __iter__(self):
+        for service in self.batch:
+            yield service
+
+    def __len__(self):
+        return len(self.batch)
+
+
+params = {'statuses[]': 3}
+services = Services()
+services.get_services_list(params)
+print(len(services))
