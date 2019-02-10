dbac8a8a54bcb71c10e1897382007eb9e47a165c
kyle <kysevenle@gmail.com>
2019-02-10 12:51:41 -0700

    reworked services class to be an iterable container class

--- ucrmapiwrap/services.py
+++ ucrmapiwrap/services.py
@@ -4,29 +4,22 @@ from ucrmapiwrap.config_reader import config
 
 
 class Services:
-    def __init__(self):
+    def __init__(self, params=None):
         self.url = config.BASE_URL + 'clients/services'
         self.headers = {'Content-Type': 'application/json',
                         'x-Auth-App-Key': config.WRITE_KEY}
-        self.batch = []
+        self._services_list = []
         self.errors = {}
 
-    def get_services_list(self, params=None):
-        self.batch = requests.get(self.url, headers=self.headers, params=params).json()
-
-    def get_service_by_id(self, id):
-        url = f'{self.url}/{id}'
-        return requests.get(url, headers=self.headers)
+        response = requests.get(self.url, headers=self.headers, params=params)
+        if response.status_code == 200:
+            self._services_list = response.json()
+        else:
+            self.errors = response.json()
 
     def __iter__(self):
-        for service in self.batch:
+        for service in self._services_list:
             yield service
 
     def __len__(self):
-        return len(self.batch)
-
-
-params = {'statuses[]': 3}
-services = Services()
-services.get_services_list(params)
-print(len(services))
+        return len(self._services_list)
