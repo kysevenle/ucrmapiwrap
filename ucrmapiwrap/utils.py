fc553c2727e35c960eeacd8e1eabf0c6a2b799fc
kysevenle <kysevenle@gmail.com>
2019-02-12 15:06:43 -0700

    changed offset param to limit, started device update

--- ucrmapiwrap/utils.py
+++ ucrmapiwrap/utils.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 from ucrmapiwrap.clients import UCRMClients
 from ucrmapiwrap.services import UCRMServices
+from ucrmapiwrap.devices import UCRMDevices
 
 
 def update_client_structs():
-    client = UCRMClients(params={'offset': 1})._clients_list[0]
+    client = UCRMClients(params={'limit': 1})._clients_list[0]
     client_struct = {k: None for k in client}
     curdir = Path(__file__).parent
     file_name = f'{curdir}\\tests\\call_structs\\client.json'
@@ -16,7 +17,16 @@ def update_client_structs():
 
 
 def update_service_structs():
-    service = UCRMServices(params={'offset': 1})._services_list[0]
+    service = UCRMServices(params={'limit': 1})._services_list[0]
+    service_struct = {k: None for k in service}
+    curdir = Path(__file__).parent
+    file_name = f'{curdir}\\tests\\call_structs\\service.json'
+    with open(file_name, 'w') as file:
+        json.dump(service_struct, file)
+
+
+def update_device_structs():
+    service = UCRMDevices()._devices_list[0]
     service_struct = {k: None for k in service}
     curdir = Path(__file__).parent
     file_name = f'{curdir}\\tests\\call_structs\\service.json'
