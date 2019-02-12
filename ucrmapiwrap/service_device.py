306fdba100774b6547a5f8bf9cc8029671b48e8e
kysevenle <kysevenle@gmail.com>
2019-02-12 16:45:14 -0700

    fixed typos on pingnotifications and url

--- ucrmapiwrap/service_device.py
+++ ucrmapiwrap/service_device.py
@@ -4,12 +4,12 @@ from ucrmapiwrap.config_reader import config
 
 
 class UCRMServiceDevice:
-    url = config.BASE_URL + 'clients/services/service_devices/'
+    url = config.BASE_URL + 'clients/services/service-devices'
     headers = {'Content-Type': 'application/json',
                'x-Auth-App-Key': config.WRITE_KEY}
 
     def __init__(self, interfaceId, vendorId, ipRange, macAddress, loginUsername, sshPort,
-                 sendPingNotifictions, pingNotificationUserId, createPingStatistics,
+                 sendPingNotifications, pingNotificationUserId, createPingStatistics,
                  createSignalStatistics, qosEnabled, qosDeviceIds, id, serviceId,
                  loginPassword):
         self.interfaceId = interfaceId
@@ -18,7 +18,7 @@ class UCRMServiceDevice:
         self.macAddress = macAddress
         self.loginUsername = loginUsername
         self.sshPort = sshPort
-        self.sendPingNotifictions = sendPingNotifictions
+        self.sendPingNotifications = sendPingNotifications
         self.pingNotificationUserId = pingNotificationUserId
         self.createPingStatistics = createPingStatistics
         self.createSignalStatistics = createSignalStatistics
