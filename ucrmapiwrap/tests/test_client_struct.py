1115c1e50cafea2e1f433f38d27a647891ad6d26
kysevenle <kysevenle@gmail.com>
2019-02-12 13:34:00 -0700

    create test to test client structure has not changed

---
+++ ucrmapiwrap/tests/test_client_struct.py
@@ -0,0 +1,25 @@
+import json
+import pytest
+
+from pathlib import Path
+
+from ucrmapiwrap.clients import UCRMClients
+
+
+@pytest.fixture
+def client_from_api():
+    client = UCRMClients()._clients_list[0]
+    return [*client]
+
+
+@pytest.fixture
+def client_from_json():
+    cur_dir = Path(__file__).parent
+    file_name = f'{cur_dir}\\call_structs\\client.json'
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+    return [*data]
+
+
+def test_client_structure(client_from_api, client_from_json):
+    assert client_from_api == client_from_json
