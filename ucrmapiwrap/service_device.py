42d1434324c76daf8df8ec24fac878e02bafbd82
kyle <kysevenle@gmail.com>
2019-02-11 20:26:30 -0700

    Created ServiceDevice class

---
+++ ucrmapiwrap/service_device.py
@@ -0,0 +1,36 @@
+import requests
+
+from ucrmapiwrap.config_reader import config
+
+
+class ServiceDevice:
+    url = config.BASE_URL + 'clients/services/service_devices/'
+    headers = {'Content-Type': 'application/json',
+               'x-Auth-App-Key': config.WRITE_KEY}
+
+    def __init__(self, interfaceId, vendorId, ipRange, macAddress, loginUsername, sshPort,
+                 sendPingNotifictions, pingNotificationUserId, createPingStatistics,
+                 createSignalStatistics, qosEnabled, qosDeviceIds, id, serviceId,
+                 loginPassword):
+        self.interfaceId = interfaceId
+        self.vendorId = vendorId
+        self.ipRange = ipRange
+        self.macAddress = macAddress
+        self.loginUsername = loginUsername
+        self.sshPort = sshPort
+        self.sendPingNotifictions = sendPingNotifictions
+        self.pingNotificationUserId = pingNotificationUserId
+        self.createPingStatistics = createPingStatistics
+        self.createSignalStatistics = createSignalStatistics
+        self.qosEnabled = qosEnabled
+        self.qosDeviceIds = qosDeviceIds
+        self.id = id
+        self.serviceId = serviceId
+        self.loginPassword = loginPassword
+
+    @classmethod
+    def from_api(cls, id):
+        url = f'{cls.url}/{id}'
+        response = requests.get(url, headers=cls.headers)
+        if response.status_code == 200:
+            return cls(**response.json())
