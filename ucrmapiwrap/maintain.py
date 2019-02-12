1e52b649204e0dfc339c9182dc5f0c20621ec13f
kysevenle <kysevenle@gmail.com>
2019-02-12 16:46:12 -0700

    started remove_signal_stats maintainance script

---
+++ ucrmapiwrap/maintain.py
@@ -0,0 +1,10 @@
+from ucrmapiwrap.services import UCRMServices
+from ucrmapiwrap.service_device import UCRMServiceDevice
+from ucrmapiwrap.service_devices import UCRMServiceDevices
+
+
+def remove_signal_stats():
+    services = UCRMServices(params={'statuses[]': [1, 3]})
+    service_devices = [UCRMServiceDevice(**service_device) for service in services for service_device in UCRMServiceDevices(service.id)._service_devices_list]
+    for service_device in service_devices:
+        print(service_device.id)
