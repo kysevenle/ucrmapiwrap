a2068a0af562a6822644cde989c432afb6aad7a8
kyle <kysevenle@gmail.com>
2019-02-11 20:46:56 -0700

    Created Devices class

---
+++ ucrmapiwrap/devices.py
@@ -0,0 +1,26 @@
+import requests
+
+from ucrmapiwrap.config_reader import config
+from ucrmapiwrap.device import Device
+
+
+class Devices:
+    def __init__(self):
+        self.url = config.BASE_URL + 'devices'
+        self.headers = {'Content-Type': 'application/json',
+                        'x-Auth-App-Key': config.WRITE_KEY}
+        self._devices_list = []
+        self.errors = {}
+
+        response = requests.get(self.url, headers=self.headers)
+        if response.status_code == 200:
+            self._devices_list = response.json()
+        else:
+            self.errors = response.json()
+
+        def __iter__(self):
+            for device in self._devices_list:
+                yield Device(**device)
+
+        def __len__(self):
+            return len(self._devices_list)
