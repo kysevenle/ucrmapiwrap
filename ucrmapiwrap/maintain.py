3936e565d3f944a6f5cbc93ed577806ccd04f236
kyle <kysevenle@gmail.com>
2019-02-12 21:14:46 -0700

    changes to list comprehension

--- ucrmapiwrap/maintain.py
+++ ucrmapiwrap/maintain.py
@@ -5,6 +5,9 @@ from ucrmapiwrap.service_devices import UCRMServiceDevices
 
 def remove_signal_stats():
     services = UCRMServices(params={'statuses[]': [1, 3]})
-    service_devices = [UCRMServiceDevice(**service_device) for service in services for service_device in UCRMServiceDevices(service.id)._service_devices_list]
+    service_devices = [UCRMServiceDevice(**service_device) for service in services
+                       for service_device
+                       in UCRMServiceDevices(service.id)._service_devices_list
+                       if service_device['createSignalStatistics'] is True]
     for service_device in service_devices:
         print(service_device.id)
