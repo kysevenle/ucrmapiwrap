d0508f5d8aa998b83a96eadec8f6fe65b4cb15fa
kysevenle <kysevenle@gmail.com>
2019-02-22 16:48:16 -0700

    got remove signal stats to get a list of service devices with sigstats

--- ucrmapiwrap/maintain.py
+++ ucrmapiwrap/maintain.py
@@ -1,13 +1,11 @@
 from ucrmapiwrap.services import UCRMServices
-from ucrmapiwrap.service_device import UCRMServiceDevice
 from ucrmapiwrap.service_devices import UCRMServiceDevices
 
 
 def remove_signal_stats():
     services = UCRMServices(params={'statuses[]': [1, 3]})
-    service_devices = [UCRMServiceDevice(**service_device) for service in services
-                       for service_device
-                       in UCRMServiceDevices(service.id)._service_devices_list
-                       if service_device['createSignalStatistics'] is True]
-    for service_device in service_devices:
-        print(service_device.id)
+    service_devices = [UCRMServiceDevices(service.id) for service in services]
+    service_devices_with_ss = [service_device for service in service_devices for
+                               service_device in service
+                               if service_device.createSignalStatistics is True]
+    print(len(service_devices_with_ss))
