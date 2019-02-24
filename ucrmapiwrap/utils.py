f477399574cdd0729742f29dbd8066e85f9f774d
kyle <kysevenle@gmail.com>
2019-02-24 13:09:11 -0700

    update struct json files to work with containers containing objs

--- ucrmapiwrap/utils.py
+++ ucrmapiwrap/utils.py
@@ -3,13 +3,14 @@ import json
 from pathlib import Path
 
 from ucrmapiwrap.clients import UCRMClients
-from ucrmapiwrap.services import UCRMServices
 from ucrmapiwrap.devices import UCRMDevices
+from ucrmapiwrap.service_devices import UCRMServiceDevices
+from ucrmapiwrap.services import UCRMServices
 
 
 def update_client_structs():
-    client = UCRMClients(params={'limit': 1})._clients_list[0]
-    client_struct = {k: None for k in client}
+    client = UCRMClients(params={'limit': 1})[0]
+    client_struct = {k: None for k in vars(client)}
     curdir = Path(__file__).parent
     file_name = f'{curdir}\\tests\\call_structs\\client.json'
     with open(file_name, 'w') as file:
@@ -17,8 +18,8 @@ def update_client_structs():
 
 
 def update_service_structs():
-    service = UCRMServices(params={'limit': 1})._services_list[0]
-    service_struct = {k: None for k in service}
+    service = UCRMServices(params={'limit': 1})[0]
+    service_struct = {k: None for k in vars(service)}
     curdir = Path(__file__).parent
     file_name = f'{curdir}\\tests\\call_structs\\service.json'
     with open(file_name, 'w') as file:
@@ -26,9 +27,19 @@ def update_service_structs():
 
 
 def update_device_structs():
-    device = UCRMDevices()._devices_list[0]
-    device_struct = {k: None for k in device}
+    device = UCRMDevices()[0]
+    device_struct = {k: None for k in vars(device)}
     curdir = Path(__file__).parent
     file_name = f'{curdir}\\tests\\call_structs\\device.json'
     with open(file_name, 'w') as file:
         json.dump(device_struct, file)
+
+
+def update_service_device_structs():
+    service = UCRMServices(params={'limit': 1})[0]
+    service_device = UCRMServiceDevices(service.id)
+    service_device_struct = {k: None for k in vars(service_device)}
+    curdir = Path(__file__).parent
+    file_name = f'{curdir}\\tests\\call_structs\\service_device.json'
+    with open(file_name, 'w') as file:
+        json.dump(service_device_struct, file)
